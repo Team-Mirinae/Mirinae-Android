fe966dbab89199ee975b3992db2d5900cff1b6bc
황지민 <80872558+krapoi@users.noreply.github.com>
2022-07-14 23:42:27 +0900

    add kakao map

--- app/src/main/java/com/example/mirinae/view/MainActivity.kt
+++ app/src/main/java/com/example/mirinae/view/MainActivity.kt
@@ -3,12 +3,41 @@ package com.example.mirinae.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
 import com.example.mirinae.R
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MapView.MapViewEventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mapView = MapView(this)
+        val map = findViewById<RelativeLayout>(R.id.Map)
+        map.addView(mapView)
+
     }
+
+    override fun onMapViewInitialized(p0: MapView?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMapViewSingleTapped(p0: MapView?, p1: MapPoint?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMapViewCenterPointMoved(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewZoomLevelChanged(p0: MapView?, p1: Int) {}
+
+    override fun onMapViewDoubleTapped(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewLongPressed(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewDragStarted(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewDragEnded(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {}
 }
